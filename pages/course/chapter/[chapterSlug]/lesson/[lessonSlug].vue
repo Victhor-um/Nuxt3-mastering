5b70f97b7799059e674a203a3e9ca4e158c9ea5f
Victor <uberman59@gmail.com>
2023-07-03 19:32:46 +0500

    add course data

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,8 +1,26 @@
 <template>
   <h3>Lesson</h3>
   <span>This a lesson</span>
+  <p>{{ chapter?.title }}</p>
+  <p>{{ lesson?.title }}</p>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const course = useCourse();
+console.log('🚀 ~ file: [lessonSlug].vue:10 ~ course:', course);
+const route = useRoute();
+
+const chapter = computed(() => {
+  return course.chapters.find(
+    (chapter) => chapter.slug === route.params.chapterSlug
+  );
+});
+
+const lesson = computed(() => {
+  return chapter.value.lessons.find(
+    (lesson) => lesson.slug === route.params.lessonSlug
+  );
+});
+</script>
 
 <style scoped></style>
