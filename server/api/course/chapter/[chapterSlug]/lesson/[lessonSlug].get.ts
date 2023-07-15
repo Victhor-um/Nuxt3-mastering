e05450104730ddcb878e2f59ea3b6cd3de9ecb0b
Victor <uberman59@gmail.com>
2023-07-15 19:31:50 +0500

    add types and first server route

--- server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
+++ server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
@@ -1,27 +1,34 @@
+import { Course, Chapter, Lesson, LessonWithPath } from '~/types/course';
+
 import course from '~/server/courseData';
-import { computed } from 'vue';
-export default defineEventHandler((event) => {
+course as Course;
+
+export default defineEventHandler((event): LessonWithPath => {
   const { chapterSlug, lessonSlug } = event.context.params;
 
-  const chapter = computed(() => {
-    return course.chapters.find((chapter) => chapter.slug === chapterSlug);
-  });
+  const chapter: Maybe<Chapter> = course.chapters.find(
+    (chapter) => chapter.slug === chapterSlug
+  );
 
-  if (!chapter.value) {
+  if (!chapter) {
     throw createError({
       statusCode: 404,
       message: 'Chapter not found',
     });
   }
-  const lesson = computed(() => {
-    return chapter.value.lessons.find((lesson) => lesson.slug === lessonSlug);
-  });
-  if (!lesson.value) {
+  const lesson: Maybe<Lesson> = chapter.lessons.find(
+    (lesson) => lesson.slug === lessonSlug
+  );
+
+  if (!lesson) {
     throw createError({
       statusCode: 404,
       message: 'Lesson not found',
     });
   }
 
-  return lesson.value;
+  return {
+    ...lesson,
+    path: `/course/chapter/${chapterSlug}/lesson/${lessonSlug}`,
+  };
 });
