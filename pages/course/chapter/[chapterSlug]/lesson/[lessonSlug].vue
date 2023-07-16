f17be1294fa06310f04aced09543eec77efbe96e
Victor <uberman59@gmail.com>
2023-07-16 12:50:41 +0500

    add useLesson and useFetchWithCache

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -30,6 +30,8 @@
 <script setup lang="ts">
 const course = useCourse();
 const route = useRoute();
+const { chapterSlug, lessonSlug } = route.params;
+const lesson = await useLesson(chapterSlug, lessonSlug);
 
 definePageMeta({
   middleware: [
@@ -74,12 +76,6 @@ const progress = useLocalStorage('progress', () => {
 const chapter = computed(() => {
   return course.chapters.find(
     (chapter) => chapter.slug === route.params.chapterSlug
-  );
-});
-
-const lesson = computed(() => {
-  return chapter.value.lessons.find(
-    (lesson) => lesson.slug === route.params.lessonSlug
   );
 });
 
